227f85b544215b7ffa444bf42aa8bda633ede876
TH3HACKTF <168471099+GustavoUA@users.noreply.github.com>
2025-12-08 23:09:21 +0000

    Add login and register pages

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -21,7 +21,7 @@ export default function RegisterPage() {
       email,
       password,
       options: {
-        emailRedirectTo: `${window.location.origin}/login`,
+        emailRedirectTo: `${window.location.origin}./login`,
       },
     });
 
